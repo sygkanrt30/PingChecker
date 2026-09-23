525e66efa2566e7c44520e600888ee732e2107f9
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-09-23 17:16:11 +0300

    polishing

--- pingChecker.ps1
+++ pingChecker.ps1
@@ -1,14 +1,9 @@
-#Requires -Version 5.1
-
+#Requires -Version 7.0
 <#
 .NOTES
     Автор: Vyacheslav Yanin
     Версия: 1.0.0
 #>
-
-if ($PSVersionTable.PSVersion.Major < 7) {
-    pwsh.exe
-}
 
 $ping = Test-Connection 8.8.8.8
 
